8fe7ce6e6e5704288da735ab5a03f9eaa789742a
cassandra <cassandradaw@gmail.com>
2018-07-25 16:27:01 +1000

    final styling on the Sell Art Page

--- client/src/pages/SellArt.js
+++ client/src/pages/SellArt.js
@@ -9,16 +9,18 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 export default () => (<div>
     <h2 className="PageHeader">Apply To Sell Art</h2>
         <div className="historytextcolour"><hr /> 
-            <h3 className="PageHeader">A website for Artists to bring there work to you</h3>
-                    
+        <h3 className="PageHeader">A website for Artists to bring their work to you</h3>  
+
             <span className="GoToShopButton">   
                 <a href="/sellart"><Button outline color="info" size="lg">Apply Now</Button></a>
-            </span> 
-                
+            </span>
+            
             <div className="producttext">
-            <p className="PageHeader">ESP Gallery is here to help show your work to the world. We have years of experiance and are dedicated to showcaseing artist’s work. Your art should be easily accessable by everyone.</p>
+            <p className="PageHeader">ESP Gallery is here to help show your work to the world.<br /><br /> We have years of experiance and are dedicated to showcaseing artist’s work.<br /><br /> Your art should be easily accessable by everyone.</p>
             <h4 className="PageHeader">We are here to bring your art to the world</h4>
             </div><br />
+        
+                
       
             <p className="abouttext historytextcolour" > Hell of helvetica chia, swag sed humblebrag cray prism aesthetic meditation shabby chic pabst. Tattooed tousled craft beer banjo elit. Try-hard eu chillwave jean shorts bushwick kombucha coloring book ut. Artisan ennui 90's dolore cupidatat church-key butcher wayfarers culpa bushwick distillery lumbersexual fam unicorn. Narwhal fingerstache YOLO iceland migas bushwick nostrud pitchfork enamel pin la croix eu commodo neutra. Intelligentsia portland in woke lyft.<br /><br />
             Single-origin coffee flannel viral voluptate incididunt, wolf quinoa sed mlkshk 8-bit thundercats DIY freegan edison bulb tilde. Chia pop-up tempor occaecat normcore pork belly eiusmod brooklyn banh mi umami direct trade. Cornhole anim messenger bag aliqua echo park aute mlkshk synth irure scenester gastropub leggings enamel pin PBR&B snackwave. Pitchfork skateboard glossier lomo. Activated charcoal gluten-free literally irure coloring book enim chambray dreamcatcher health goth sed occupy.</p><hr />
